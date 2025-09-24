6a26f0db2682a079a2035cd5180b64dd953ff5b5
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-09-24 16:31:09 -0500

    Primitives within a session and local testing examples.

--- first-rsa-attack/Example01.py
+++ first-rsa-attack/Example01.py
@@ -1,14 +1,72 @@
-from qiskit_ibm_runtime import QiskitRuntimeService, Session
+# Primitives within a session #
+
+#
+# Computation elements
+
+# Import the Sampler primitive for running Qiskit
 from qiskit_ibm_runtime import SamplerV2 as Sampler
+# Same for the Estimator primitive
 from qiskit_ibm_runtime import EstimatorV2 as Estimator
+# Import the QiskitRuntimeService and Session classes
+from qiskit_ibm_runtime import QiskitRuntimeService, Session
+# Import the QiskitRuntimeService and Session classes
+from qiskit_ibm_runtime import QiskitRuntimeService, Session
+# Import the real_amplitudes circuit toolkit
 from qiskit.circuit.library import real_amplitudes
-from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
+# Import the registers and the circuit class
+from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
+# Import the Pauli operator
 from qiskit.quantum_info import SparsePauliOp
+# From the compiler (transpiler), import the pass manager
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
-# Initialize session
-service = QiskitRuntimeService()
+#
+# Constructing the job
 
-# Prepare inputs
-psi = real_amplitudes(num_qubits=2, reps=2)
-H1 = SparsePauliOp.from_list([])
+myservice= QiskitRuntimeService()
+# Get a wave function
+psi= real_amplitudes(num_qubits=2, reps=2)
+# Get a Hamiltonian
+H1= SparsePauliOp.from_list( [("II", 1), ("IZ", 2), ("XI", 3)] )
+# List argument theta
+theta= [0, 1, 1, 2, 3, 5]
+# Constructing the circuit
+# Quantum register with two fields
+qr= QuantumRegister(2, name= "qr")
+# Classical register with two fields
+cr= ClassicalRegister(2, name= "cr")
+# The quantum circuit constructor recieves both registers as arguments
+qc= QuantumCircuit(qr, cr, name= "may")
+qc.h(qr[0])
+qc.cx(qr[0], qr[1])
+qc.measure(qr, cr)
+
+#
+# Job elements
+
+mybackend= myservice.least_busy(operational= True, simulator= False)
+# Get pass manager, which coerces ISA requirements on the circuit components
+pm= generate_preset_pass_manager(target= mybackend.target, optimization_level= 1)
+
+#
+# Meet ISA requirements
+
+may_isa= pm.run(qc)
+psi_isa= pm.run(psi)
+# Observables that are ISA
+observables= H1.apply_layout(psi_isa.layout)
+
+#
+# Running job with Sampler
+
+mysession= Session(backend= mybackend)
+mysampler= Sampler(mode= mysession)
+myjob= mysampler.run([may_isa])
+myresult= myjob.result()[0]
+
+#
+# Running job with Estimator
+
+myestimator= Estimator(mode= mysession)
+myjob= myestimator.run([psi_isa, observables, theta])
+myresult= myjob.result()[0]
--- first-rsa-attack/Example02.py
+++ first-rsa-attack/Example02.py
@@ -1,8 +1,57 @@
+# For local testing #
+
+#
+# Backend components
+
+# Import Qiskit Aer: backend whose instances permit local testing
 from qiskit_aer import AerSimulator
-# Again import the real_amplitudes circuit toolkit
-from qiskit.circuit.library import real_amplitudes
+# Import the real_amplitudes circuit toolkit
+from qiskit.circuit.Qiskit Aer: backend import real_amplitudes
 # We will again initialize the quantum circuit with a quantum register and a
 # classical register
 from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
 # Import the Pauli operator
 from qiskit.quantum_info import SparsePauliOp
+# Import pass manager
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+#
+# Runtime tools
+
+from qiskit_ibm_runtime import Session
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+# Import fake provider
+from qiskit_ibm_runtime.fake_provider import FakeManilaV2
+
+#
+# Initialize quantum circuit
+
+qc= QuantumCircuit(2)
+qc.h(0)
+qc.cx(0,1)
+qc.measure_all()
+
+#
+# Running job locally with FakeManila
+
+# Get fake provider
+myfakeprov= FakeManilaV2()
+# Get pass manager
+pm= generate_preset_pass_manager(backend= myfakeprov, optimization_level= 1)
+# Make qc meet isa requirements
+isa_qc= pm.run(qc)
+# Get the primitive
+mysampler= Sampler(backend= myfakeprov)
+# Run with Sampler primitive and get result
+result= mysampler.run([isa_qc]).result()
+
+#
+# Running locally with AerSimulator
+aer_sim= AerSimulator()
+# Get passmanager with aer for backend
+pm= generate_preset_pass_manager(backend= aer_sim, optimization_level= 1)
+isa_qc= pm.run(qc)
+# Session syntax supported but ignored
+mysession= Session(backend= aer_sim)
+sampler= Sampler(mode= session)
+result= sampler.run([isa_qc]).result()
